08f0565b93b6d364ef1a651d2a0e86bcb330c59e
Vinicius Silva <vsaguiar@outlook.com>
2023-09-23 17:44:39 -0300

    Criando o modelo de domínio

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+
+    // Uma categoria pode ter mais de um produto
+    public ICollection<Produto> Produtos { get; set; }
+}
