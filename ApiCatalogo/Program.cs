1d6ed8a90ffcdd1ad4717b02e3599b77fee6c757
Vinicius Silva <vsaguiar@outlook.com>
2023-09-30 15:55:56 -0300

    Método de extensão para os endpoints Produtos e Categorias

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,6 +1,5 @@
 using ApiCatalogo.ApiEndpoints;
 using ApiCatalogo.Context;
-using ApiCatalogo.Models;
 using ApiCatalogo.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -74,126 +73,10 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// ---------------------------------------- ENDPOINTS ----------------------------------------
-
+// ------------------------------ ENDPOINTS ------------------------------
 app.MapAutenticacaoEndpoints();
-
-
-
-
-app.MapGet("/", () => $"Catálogo de Produtos - {DateTime.Now.Year}").ExcludeFromDescription();
-
-// ------------------- Categoria -------------------
-app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
-{
-    db.Categorias.Add(categoria);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
-});
-
-app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync()).WithTags("Categorias").RequireAuthorization();
-
-app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Categorias.FindAsync(id)
-                 is Categoria categoria
-                 ? Results.Ok(categoria)
-                 : Results.NotFound();
-});
-
-app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
-{
-    if (categoria.CategoriaId != id)
-    {
-        return Results.BadRequest();
-    }
-
-    var categoriaDB = await db.Categorias.FindAsync(id);
-    if (categoriaDB is null)
-    {
-        return Results.NotFound();
-    }
-
-    categoriaDB.Nome = categoria.Nome;
-    categoriaDB.Descricao = categoria.Descricao;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(categoriaDB);
-});
-
-app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    var categoria = await db.Categorias.FindAsync(id);
-    if (categoria is null)
-    {
-        return Results.NotFound();
-    }
-
-    db.Categorias.Remove(categoria);
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-});
-
-
-
-
-// ------------------- Produto -------------------
-app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
-{
-    db.Produtos.Add(produto);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/produtos/{produto.ProdutoId}", produto);
-});
-
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
-
-app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Produtos.FindAsync(id)
-                 is Produto produto
-                 ? Results.Ok(produto)
-                 : Results.NotFound();
-});
-
-app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
-{
-    if (produto.ProdutoId != id)
-    {
-        return Results.BadRequest();
-    }
-
-    var produtoDB = await db.Produtos.FindAsync(id);
-    if (produtoDB is null)
-    {
-        return Results.NotFound();
-    }
-
-    produtoDB.Nome = produto.Nome;
-    produtoDB.Descricao = produto.Descricao;
-    produtoDB.Preco = produto.Preco;
-    produtoDB.Imagem = produto.Imagem;
-    produtoDB.DataCompra = produto.DataCompra;
-    produtoDB.Estoque = produto.Estoque;
-    produtoDB.CategoriaId = produto.CategoriaId;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(produtoDB);
-});
-
-app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    var produto = await db.Produtos.FindAsync(id);
-    if (produto is null)
-    {
-        return Results.NotFound();
-    }
-
-    db.Produtos.Remove(produto);
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-});
+app.MapCategoriasEndpoints();
+app.MapProdutosEndpoints();
 
 
 
