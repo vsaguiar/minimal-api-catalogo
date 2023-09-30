f395e09a6b54122f8121e507a922b3d29f2bb9cf
Vinicius Silva <vsaguiar@outlook.com>
2023-09-30 15:30:48 -0300

    Método de extensão para o endpoint Login

---
+++ ApiCatalogo/ApiEndpoints/AutenticacaoEndpoints.cs
@@ -0,0 +1,32 @@
+﻿using ApiCatalogo.Models;
+using ApiCatalogo.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ApiCatalogo.ApiEndpoints;
+
+public static class AutenticacaoEndpoints
+{
+    public static void MapAutenticacaoEndpoints(this WebApplication app)
+    {
+        // Endpoint para login
+        app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
+        {
+            if (userModel is null)
+            {
+                return Results.BadRequest("Login Inválido!");
+            }
+            if (userModel.UserName == "vinicius" && userModel.Password == "123456")
+            {
+                var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+                    app.Configuration["Jwt:Issuer"],
+                    app.Configuration["Jwt:Audience"],
+                    userModel);
+                return Results.Ok(new { token = tokenString });
+            }
+            else
+            {
+                return Results.BadRequest("Login Inválido!");
+            }
+        }).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status200OK).WithName("Login").WithTags("Atenticação");
+    }
+}
--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,8 +1,8 @@
+using ApiCatalogo.ApiEndpoints;
 using ApiCatalogo.Context;
 using ApiCatalogo.Models;
 using ApiCatalogo.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -71,33 +71,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 builder.Services.AddAuthorization();
 
 
+
 var app = builder.Build();
-
-
-
 
 // ---------------------------------------- ENDPOINTS ----------------------------------------
 
-// Endpoint para login
-app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
-{
-    if (userModel is null)
-    {
-        return Results.BadRequest("Login Inválido!");
-    }
-    if (userModel.UserName == "vinicius" && userModel.Password == "123456")
-    {
-        var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
-            app.Configuration["Jwt:Issuer"],
-            app.Configuration["Jwt:Audience"],
-            userModel);
-        return Results.Ok(new { token = tokenString });
-    }
-    else
-    {
-        return Results.BadRequest("Login Inválido!");
-    }
-}).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status200OK).WithName("Login").WithTags("Atenticação");
+app.MapAutenticacaoEndpoints();
 
 
 
