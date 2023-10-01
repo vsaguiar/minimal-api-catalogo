4c8721c0a8a2dba688dfe23986de8623ec9f775f
Vinicius Silva <vsaguiar@outlook.com>
2023-10-01 18:29:16 -0300

    Configurando o serviço e incluindo o Swagger no container DI

---
+++ ApiCatalogo/AppServicesExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace ApiCatalogo.AppServicesExtensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwagger();
+        return builder;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = @"JWT Authorization header using the Bearer scheme.
+                        Enter 'Bearer'[space] Example: \'Bearer 12345abcdef\'",
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+        return services;
+    }
+}
--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -12,36 +12,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
 
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = @"JWT Authorization header using the Bearer scheme.
-                        Enter 'Bearer'[space] Example: \'Bearer 12345abcdef\'",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
 
 // Obtendo a string de conexão
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
